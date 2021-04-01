4b31570bc65c3b754456e8b4e179e4c57300d45e
Sergey <zakharov.nsg@gmail.com>
2021-04-02 01:03:19 +0500

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            fallback: require.resolve("file-loader"),
+            esModule: false,
+          },
+          // loader: "file-loader",
+          // options: {
+          //   name: "[name].[hash].[ext]",
+          //   outputPath: "imgs",
+          //   esModule: false,
+          // },
+        },
+      },
+    ],
+  },
+};
