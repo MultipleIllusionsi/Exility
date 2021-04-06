b0e442bb59ed73ab589a4227f1895776aac8b5d9
Sergey <zakharov.nsg@gmail.com>
2021-04-06 20:30:38 +0500

    add two sections and slider script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./scss/main.scss";
 
-console.log(`hey`);
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+const sliderWrapper = document.getElementById("slider");
+
+const getCardItemDimensions = () => {
+  const advantagesItem = document.getElementsByClassName(
+    "advantages__cards__item"
+  )[0];
+  const advantagesItemStyles = window.getComputedStyle(advantagesItem);
+  const itemOffset =
+    Number(advantagesItemStyles.marginRight.replace(/[^-\d.]/g, "")) +
+    Number(advantagesItemStyles.width.replace(/[^-\d.]/g, ""));
+
+  return itemOffset;
+};
+
+const itemOffset = getCardItemDimensions();
+
+let position = 0;
+
+rightArrow.addEventListener("click", () => {
+  if (position === 1) return;
+  const coord = sliderWrapper.style.transform.replace(/[^-\d.]/g, "");
+  sliderWrapper.style.transform = `translateX(${Number(coord) - itemOffset}px)`;
+
+  position++;
+});
+
+leftArrow.addEventListener("click", () => {
+  if (position === 0) return;
+  const coord = sliderWrapper.style.transform.replace(/[^-\d.]/g, "");
+  sliderWrapper.style.transform = `translateX(${Number(coord) + itemOffset}px)`;
+
+  position--;
+});
